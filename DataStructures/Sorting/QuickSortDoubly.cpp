1a13b2678066aa01e5f1021f314830dedec465ca
Ausaaf Nabi <nabiausaaf@gmail.com>
2020-09-15 23:48:39 +0530

    Update QuickSortDoubly.cpp

--- DataStructures/Sorting/QuickSortDoubly.cpp
+++ DataStructures/Sorting/QuickSortDoubly.cpp
@@ -65,9 +65,7 @@ void _quickSort(Node* l, Node *h)
     }  
 }  
   
-// The main function to sort a linked list. 
-// It mainly calls _quickSort()  
-void quickSort(Node *head)  
+ void quickSort(Node *head)  
 {  
     // Find last node  
     Node *h = lastNode(head);  
@@ -75,4 +73,56 @@ void quickSort(Node *head)
     // Call the recursive QuickSort  
     _quickSort(head, h);  
 }  
- 
+  
+// A utility function to print contents of arr  
+void printList(Node *head)  
+{  
+    while (head)  
+    {  
+        cout << head->data << " ";  
+        head = head->next;  
+    }  
+    cout << endl;  
+}  
+  
+/* Function to insert a node at the  
+beginging of the Doubly Linked List */
+void push(Node** head_ref, int new_data)  
+{  
+    Node* new_node = new Node; /* allocate node */
+    new_node->data = new_data;  
+  
+    /* since we are adding at the 
+    beginning, prev is always NULL */
+    new_node->prev = NULL;  
+  
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);  
+  
+    /* change prev of head node to new node */
+    if ((*head_ref) != NULL) (*head_ref)->prev = new_node ;  
+  
+    /* move the head to point to the new node */
+    (*head_ref) = new_node;  
+}  
+  
+/* Driver code */
+int main()  
+{  
+    Node *a = NULL;  
+    push(&a, 5);  
+    push(&a, 20);  
+    push(&a, 4);  
+    push(&a, 3);  
+    push(&a, 30);  
+  
+    cout << "Linked List before sorting \n";  
+    printList(a);  
+  
+    quickSort(a);  
+  
+    cout << "Linked List after sorting \n";  
+    printList(a);  
+  
+    return 0;  
+} 
